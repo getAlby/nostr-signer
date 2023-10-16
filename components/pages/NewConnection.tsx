7f24b50e002664d11c8d8910c52848a5cff14277
Roland Bewick <roland.bewick@gmail.com>
2023-10-16 15:49:23 +0700

    feat: show connecting status, add timeout

--- components/pages/NewConnection.tsx
+++ components/pages/NewConnection.tsx
@@ -1,7 +1,7 @@
 import { Link, router } from "expo-router";
 import { Page } from "../Page";
 import { Text } from "../Text";
-import { Button, StyleSheet, TextInput } from "react-native";
+import { ActivityIndicator, Button, StyleSheet, TextInput } from "react-native";
 import React from "react";
 import Toast from "react-native-toast-message";
 import { parseNostrConnectUrl } from "../../lib/parseNostrConnectUrl";
@@ -11,17 +11,35 @@ import { registerAppWithNip46PushServer } from "../../lib/registerAppWithNip46Pu
 import { sendNip46ConnectRequest } from "../../lib/sendNip46ConnectRequest";
 
 export function NewConnection() {
+  const [connectStatus, setConnectStatus] = React.useState("");
+  const [isLoading, setLoading] = React.useState(false);
   const [text, setText] = React.useState(
     "nostrconnect://b889ff5b1513b641e2a139f661a661364979c5beee91842f8f0ef42ab558e9d4?metadata=%7B%22name%22%3A%22Example%22%2C%22description%22%3A%22%F0%9F%94%89%F0%9F%94%89%F0%9F%94%89%22%2C%22url%22%3A%22https%3A%2F%2Fexample.com%22%2C%22icons%22%3A%5B%22https%3A%2F%2Fexample.com%2Ficon.png%22%5D%7D&relay=wss%3A%2F%2Frelay.damus.io"
+    //"nostrconnect://6c83959af5ed9bc73d19aa702b7e9752b643815416d1109f055552b44e4d373b?metadata=%7B%22name%22%3A%22Example%22%2C%22description%22%3A%22%F0%9F%94%89%F0%9F%94%89%F0%9F%94%89%22%2C%22url%22%3A%22https%3A%2F%2Fexample.com%22%2C%22icons%22%3A%5B%22https%3A%2F%2Fexample.com%2Ficon.png%22%5D%7D&relay=wss%3A%2F%2Frelay.damus.io"
   );
 
   async function connect() {
     const appConnection = parseNostrConnectUrl(text);
 
-    await sendNip46ConnectRequest(appConnection);
-    await store.addAppConnection(appConnection);
-    const expoToken = await registerExpoPushToken();
-    await registerAppWithNip46PushServer(expoToken, appConnection.publicKey);
+    try {
+      setConnectStatus("Sending NIP-46 connect request");
+      setLoading(true);
+      await sendNip46ConnectRequest(appConnection);
+      await store.addAppConnection(appConnection);
+      setConnectStatus("Registering push notifications");
+      const expoToken = await registerExpoPushToken();
+      setConnectStatus("Registering with NIP-46 push server");
+      await registerAppWithNip46PushServer(expoToken, appConnection.publicKey);
+    } catch (error) {
+      console.error(error);
+      Toast.show({
+        type: "error",
+        text1: "Failed to connect to app",
+        text2: (error as Error).message,
+      });
+      setLoading(false);
+      return;
+    }
 
     router.replace("/");
     Toast.show({
@@ -35,18 +53,27 @@ export function NewConnection() {
     <Page>
       <Text>New Connection</Text>
 
-      <TextInput
-        style={styles.input}
-        onChangeText={(_text) => setText(_text)}
-        value={text}
-        multiline
-      />
+      {isLoading ? (
+        <>
+          <ActivityIndicator size="large" style={{ marginVertical: 40 }} />
+          <Text>{connectStatus}</Text>
+        </>
+      ) : (
+        <>
+          <TextInput
+            style={styles.input}
+            onChangeText={(_text) => setText(_text)}
+            value={text}
+            multiline
+          />
 
-      <Button onPress={connect} title="CONNECT" />
+          <Button onPress={connect} title="CONNECT" />
 
-      <Link href={{ pathname: "/" }} style={{ marginTop: 40 }}>
-        <Text>Home</Text>
-      </Link>
+          <Link href={{ pathname: "/" }} style={{ marginTop: 40 }}>
+            <Text>Home</Text>
+          </Link>
+        </>
+      )}
     </Page>
   );
 }
--- lib/sendNip46ConnectRequest.ts
+++ lib/sendNip46ConnectRequest.ts
@@ -3,24 +3,30 @@ import { NostrSigner } from "./nostr";
 import { getPublicKey } from "nostr-tools";
 
 export async function sendNip46ConnectRequest(appConnection: AppConnection) {
-  try {
-    const privateKey = await store.getPrivateKey();
-    const handler = new NostrSigner({
-      secretKey: privateKey,
-      relay: appConnection.relay,
-    });
+  return new Promise<void>(async (resolve, reject) => {
+    setTimeout(() => {
+      reject("Timeout connecting to app");
+    }, 10000);
+    try {
+      const privateKey = await store.getPrivateKey();
+      const handler = new NostrSigner({
+        secretKey: privateKey,
+        relay: appConnection.relay,
+      });
 
-    const result = await handler.call({
-      target: appConnection.publicKey,
-      request: {
-        method: "connect",
-        params: [getPublicKey(privateKey)],
-      },
-    });
-    console.log("REQUEST RESULT:", result);
-    await handler.disconnect();
-  } catch (error) {
-    console.error("Connection failed", error);
-    throw error;
-  }
+      const result = await handler.call({
+        target: appConnection.publicKey,
+        request: {
+          method: "connect",
+          params: [getPublicKey(privateKey)],
+        },
+      });
+      console.log("REQUEST RESULT:", result);
+      await handler.disconnect();
+      resolve();
+    } catch (error) {
+      console.error("Connection failed", error);
+      throw error;
+    }
+  });
 }
